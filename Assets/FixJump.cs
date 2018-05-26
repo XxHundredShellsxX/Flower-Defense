846b0fec865bd41c67923f554ba1ff80fc966d59
Sajid <potatorahman@gmail.com>
2018-05-26 03:24:56 -0400

    First commit

---
+++ Assets/FixJump.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixJump : MonoBehaviour {
+
+    public float fallMultiplier = 4f;
+    public float lowJumpMultiplier = 2f;
+
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        // otherwise the jump speed is normal as its going up
+        else if (rb.velocity.y > 0 && !Input.GetButtonDown("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    // Use this for initialization
+    public float moveSpeed = 10f;
+    private bool faceRight = true;
+    public float moveX;
+    
+    public float jumpVelocity = 10f;
+
+    public LayerMask groundLayer;
+
+    Rigidbody2D rb;
+
+	void Awake () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+    
+	// Update is called once per frame
+	void Update () {
+        moveX = Input.GetAxis("Horizontal");
+        if(moveX > 0.0f)
+        {
+            Flip();
+        }
+        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+        // make the jump faster when player is falling
+        
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            rb.velocity = Vector2.up * jumpVelocity;
+        }
+        
+    }
+
+    bool isGrounded()
+    {
+        Vector2 position = transform.position;
+        Vector2 direction = Vector2.down;
+        float dist = 1.0f;
+        Debug.DrawRay(position, direction, Color.green);
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, dist, groundLayer);
+        if(hit.collider != null)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void Flip()
+
+    {
+
+    }
+}
